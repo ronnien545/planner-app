195db08b2fe2ba33349cba479bb6a1ee7ed81050
Ronnie Noel <ronnie.noel@student.manchester.ac.uk>
2025-04-03 19:23:32 +0100

    added edit button

--- app/page.tsx
+++ app/page.tsx
@@ -12,25 +12,55 @@ import AddEntryBtn from '@/components/AddEntryBtn';
 import AddTaskList from '@/components/AddTaskList';
 import ModifyListTitle from '@/components/ModifyListTitle';
 import { AiOutlineLoading } from "react-icons/ai";
+import { CgPen } from "react-icons/cg";
+import { CgTrash } from "react-icons/cg";
+
+type EntryItem= {
+  id:number;
+  date:string;
+  title:string;
+  content:string;
+  edit:boolean;
+}
+
+type TaskList={
+  _id:string;
+  title:string;
+  newList:boolean;
+  entries: EntryItem[];
+}
+
 
 export default function Home() {
   type InputState = Record<string, string>
   const router = useRouter();
   const {data,loading,error} = useQuery(GET_LISTS);
+  const [tasklists, setTaskLists] = useState<Map<string, TaskList>>(new Map());
   const [input,setInput] = useState<InputState>({});
   const [isLoading, startTransition] = useTransition();
+  const [hoverIdRow, setHoverIdRow] = useState(''); 
+  const [hoverIdEdit, setHoverIdEdit] = useState('');
+  const [hoverIdDelete, setHoverIdDelete] = useState(''); 
+
 
   useEffect(() => {
-    if (data?.TaskLists) { 
-    data.TaskLists.forEach((item:any) => {
-      router.prefetch(`/lists/${item._id}`);
-    });
+    if (data?.TaskLists) {
+
+      const newMap:Map<string, TaskList> = new Map(data.TaskLists.map((item:any)=> [item._id,item]));
+
+      setTaskLists(newMap); 
+      data.TaskLists.forEach((item:any) => {
+        router.prefetch(`/lists/${item._id}`);
+      });
+
     }
   }, [data, router]);
 
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+
+  
 
   const handleTitleChange = (e:React.ChangeEvent<HTMLInputElement>,item_id: string) =>{
     const newTitle = e.target.value;
@@ -46,6 +76,21 @@ export default function Home() {
     }); 
   };
 
+  const onEdit = (item_id:string) =>{
+    console.log("test3"); 
+    setTaskLists(prev => {
+       const newMap = new Map(prev);
+       const curritem = prev.get(item_id);
+       newMap.set(item_id, {...(curritem||{ 
+      _id: item_id, 
+      title: '',
+      newList: false,
+      entries: [],
+    }),newList:true})
+       return newMap
+    });
+  }
+
   return (
     <section className={styles.page}>
       <h1>Pick a collection of tasks</h1>
@@ -59,15 +104,22 @@ export default function Home() {
           </tr>
         </thead>
         <tbody>
-          {data.TaskLists.map((item:any) => (
+          {Array.from(tasklists.entries()).map(([id,item]) => (
             item.newList?
-            <tr key={item._id}>
+            <tr key={id}>
               <td><input className={styles.InputTitle} type="text" defaultValue={item.title} onChange= {(e)=>handleTitleChange(e,item._id)} placeholder={'Enter title of task here.....'} /></td>
               <td><ModifyListTitle TaskListID={item._id} title={(input[item._id]||'')}/></td>
             </tr>:
-            <tr key={item._id}  onClick={() => handleRowClick(item._id)}  className={styles.TaskRow}>
-              <td>{item.title}</td>
-              <td>{item.entries.length}</td>
+            <tr key={id} className={styles.TaskRow}>
+              <td style={{ backgroundColor: hoverIdRow === id ? '#414344' : '#f3f4f6', color:hoverIdRow === id?'aliceblue':''}}  onMouseEnter={() => setHoverIdRow(id)} onMouseLeave={() => setHoverIdRow('')} onClick={() => handleRowClick(id)}>{item.title}</td>
+              <td style={{ backgroundColor: hoverIdRow === id ? '#414344' : '#f3f4f6', color:hoverIdRow === id?'aliceblue':''}} onMouseEnter={() => setHoverIdRow(id)}
+        onMouseLeave={() => setHoverIdRow('')} onClick={() => handleRowClick(id)}>{item.entries.length}</td>
+              <td><div className={styles.IconCircle}  onClick={() => onEdit(id)} style={{ backgroundColor: hoverIdEdit === id ? '#d9e00b' : '', color:hoverIdEdit === id?'aliceblue':''}} onMouseEnter={() => setHoverIdEdit(id)} onMouseLeave={() => setHoverIdEdit('')}><CgPen/>
+              </div>
+              {'   '}
+              <div className={styles.IconCircle} style={{ backgroundColor: hoverIdDelete === item._id ? '#e04b0b' : '', color:hoverIdDelete === item._id?'aliceblue':''}} onMouseEnter={() => setHoverIdDelete(item._id)} onMouseLeave={() => setHoverIdDelete('')}><CgTrash/>
+              </div>
+              </td>
             </tr>
           ))}
         </tbody>
